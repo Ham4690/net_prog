1be6f8915120864e4b28d326d2693012803d763d
Ham4690 <ham46903@gmail.com>
2019-06-09 07:59:54 +0900

    add kill fork

--- kadai_3/kadai_3/kadai_3.c
+++ kadai_3/kadai_3/kadai_3.c
@@ -3,6 +3,38 @@
 
 #define BUFSIZE 50   /* バッファサイズ */
 
+void child_process(int sock_listen,int *sock_accepted){
+  char buf[BUFSIZE];
+  int strsize;
+  for(;;){
+  /* クライアントの接続を受け付ける */
+  *sock_accepted = accept(sock_listen, NULL, NULL);
+    do{
+        /* 文字列をクライアントから受信する */
+        if((strsize=recv(*sock_accepted, buf, BUFSIZE, 0)) == -1){
+          exit_errmesg("recv()");
+        }
+
+        /* 文字列をクライアントに送信する */
+        if(send(*sock_accepted, buf, strsize, 0) == -1 ){
+          exit_errmesg("send()");
+        }
+      }while( buf[strsize-1] != '\n' ); /* 改行コードを受信するまで繰り返す */
+    close(*sock_accepted);
+  }
+}
+
+void parrent_process(pid_t child,int *child_pid,int count,int sock_accepted){
+  printf("Client is accepted.[%d]\n", child);
+  child_pid[count]=child;
+//  close(sock_accepted);
+}
+
+void failed_fork(sock_listen){
+  close(sock_listen);
+  exit_errmesg("fork()");
+}
+
 int main(int argc, char *argv[])
 {
   int port_number;
@@ -10,9 +42,9 @@ int main(int argc, char *argv[])
   int sock_listen, sock_accepted;
   int *child_pid;
   pid_t child;
-  char buf[BUFSIZE];
-  int strsize;
   int count;
+  char str[256];
+  char cmnd[256];
   int i;
 
   /* 引数のチェックと使用法の表示 */
@@ -25,49 +57,47 @@ int main(int argc, char *argv[])
   child_number = atoi(argv[2]);
   child_pid=(int *)malloc(sizeof(int)*child_number);
 
-
   /* サーバの初期化 */
   sock_listen = init_tcpserver(port_number, 5);
 
   for(count=0;count<child_number;count++){
     
     if( (child=fork() ) == 0 ){
-      /* Child process */
-    for(;;){
-
-      /* クライアントの接続を受け付ける */
-      sock_accepted = accept(sock_listen, NULL, NULL);
-//      close( sock_listen );
-      do{
-	/* 文字列をクライアントから受信する */
-            if((strsize=recv(sock_accepted, buf, BUFSIZE, 0)) == -1){
-            exit_errmesg("recv()");
-            }
-
-            /* 文字列をクライアントに送信する */
-            if(send(sock_accepted, buf, strsize, 0) == -1 ){
-            exit_errmesg("send()");
-            }
-        }while( buf[strsize-1] != '\n' ); /* 改行コードを受信するまで繰り返す */
-      close(sock_accepted);
-
-    }
-   }else if( child > 0 ){
-      /* parent's process */
-      printf("Client is accepted.[%d]\n", child);
-      child_pid[count]=child;
-      close(sock_accepted);
+      printf("test\n");
+      child_process(sock_listen,&sock_accepted);
+    }else if( child > 0 ){
+      parrent_process(child,child_pid,count,sock_accepted);
     }else{
-      /* fork()に失敗 */
-      close(sock_listen);
-      exit_errmesg("fork()");
+      failed_fork(sock_listen);
     }
+
   }
 
   for(i=0;i<count;i++){
     printf("child_pid[%d]=%d\n",i,child_pid[i]);
   }
 
+  while(1){
+    fgets(str,sizeof(str), stdin);
+    str[strlen(str)-1]='\0';
+    printf("%s\n",str);
+    if(strcmp(str,"exit")==0){
+      for(i=0;i<count;i++){
+            sprintf(cmnd,"kill %d",child_pid[i]);
+            system(cmnd);
+      }
+      break;
+    }
+  }
+
+
+  printf("kill start\n");
+
+  for(i=0;i<count;i++){
+        sprintf(cmnd,"kill %d",child_pid[i]);
+        system(cmnd);
+  }
   
+  free(child_pid);
 
 }
